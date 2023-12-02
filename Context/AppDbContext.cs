5d808b2f02a49546b6e934e308f11aeebd9babe0
Bonandi22 <elkerbonandi@gmail.com>
2023-12-02 12:14:13 +0000

    Sales_Manegement initial

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Sales_Management.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+
+
+
+    }
+}
---
+++ Models/LoginModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sales_Management.Models
+{
+    public class LoginModel
+    {
+        public string? Id { get; set; }
+
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Informe o e-mail do usuário!")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "O e-mail informado é inválido!")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Informe a senha do usuário!")]
+        public string? Password { get; set; }
+
+    }
+}
